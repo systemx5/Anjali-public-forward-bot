bc7397d6ea1a7f39f84d27f7458df680e1b76779
systemx5 <154720925+systemx5@users.noreply.github.com>
2023-12-30 19:52:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -14,8 +14,8 @@ buttons=InlineKeyboardMarkup(
                 InlineKeyboardButton("How Does This Works?", callback_data="abt")
             ],
             [
-                InlineKeyboardButton("Source Code", url="https://github.com/Greymattersbot/public-forward-bot"),
-                InlineKeyboardButton("Updates Channel", url="https://t.me/greymatter_bots")
+                InlineKeyboardButton("Source Code", url="https://github.com/systemx5/Anjali-public-forward-bot"),
+                InlineKeyboardButton("Updates Channel", url="https://t.me/+fztuVtP1frMwYTk1")
             ]
         ]
         )
@@ -64,7 +64,7 @@ async def cb_abt(bot, cb):
     reply_markup=InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("Source", url="https://github.com/Greymattersbot/public-forward-bot"),
+                InlineKeyboardButton("Source", url="https://github.com/systemx5/Anjali-public-forward-bot"),
             ]
         ]
     )
